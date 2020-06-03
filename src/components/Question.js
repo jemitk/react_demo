fc62e99f435e6d7a084a1a5c8adc361b2c81bb62
Karen Jong-Hwi Lee <jemitk15@gmail.com>
2020-06-02 22:06:09 -0700

    State not needed for individual questions, removing

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,21 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Select from 'react-select';
 
 // Component for individual questions
 const Question = (props) => {
     const {question, options, handleAnswerChange} = props;
-    const [selected, setSelected] = useState(0);
 
     const handleOnChange = (newlySelected) => {
-        console.log(newlySelected);
-        setSelected(newlySelected.value);
         handleAnswerChange(newlySelected.value);
     }
 
     return (
         <>
             <p>{question}</p>
-            <Select options={options} value={selected} onChange={handleOnChange}/>
+            <Select options={options} onChange={handleOnChange}/>
         </>
     );
 }
