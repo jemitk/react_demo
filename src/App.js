9a5fa2cfaec6b79fe257c6847360d0de62716b39
Karen Jong-Hwi Lee <jemitk15@gmail.com>
2020-06-05 17:31:43 -0700

    Hook up Complete component with App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
+import Complete from './components/Complete';
 import Questionnaire from './components/Questionnaire';
 import Welcome from './components/Welcome';
 import './App.css';
 
 const App = () => {
-    // 0 for Welcome, 1 for Questionnaire
+    // 0 for Welcome, 1 for Questionnaire, 2 for Complete
+    // TODO: Consider making an enum
     const [page, setPage] = useState(0);
 
     return (
         <>
-            {page === 0 ? <Welcome onClickStart={() => setPage(1)}/> : null}
-            {page === 1 ? <Questionnaire /> : null}
+            { page === 0 ? <Welcome onClickStart={() => setPage(1)}/> : null }
+            { page === 1 ? <Questionnaire onClickSubmit={() => setPage(2)}/> : null }
+            { page === 2 ? <Complete onClickGoBack={() => setPage(0)}/> : null }
         </>
     );
 }
